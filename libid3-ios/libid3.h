61b6231ff044e4b2f66ef7f4e926b2a7a6b98d98
Piet Brauer <piet@nerdishbynature.com>
2017-02-15 11:53:01 +0100

    Move to dynamic framework

---
+++ libid3-ios/libid3.h
@@ -0,0 +1,19 @@
+//
+//  libid3-ios.h
+//  libid3-ios
+//
+//  Created by Piet Brauer on 15/02/2017.
+//  Copyright © 2017 ラクラクテクノロジーズ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libid3-ios.
+FOUNDATION_EXPORT double libid3_iosVersionNumber;
+
+//! Project version string for libid3-ios.
+FOUNDATION_EXPORT const unsigned char libid3_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libid3_ios/PublicHeader.h>
+
+
